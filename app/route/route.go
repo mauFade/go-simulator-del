2533a5a0f5b9a98efe71daa09682eb6054c105e8
Mauricio <mauricio.cds00@gmail.com>
2023-02-06 11:13:40 -0300

    feat: fload positions method

--- app/route/route.go
+++ app/route/route.go
@@ -1,5 +1,13 @@
 package route
 
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Route struct {
 	ID        string
 	ClientID  string
@@ -9,4 +17,43 @@ type Route struct {
 type Position struct {
 	Latitude  float64
 	Longitude float64
+}
+
+func (route *Route) LoadPositions() error {
+	if route.ID == "" {
+		return errors.New("Missing route ID.")
+	}
+
+	file, err := os.Open("destinations/" + route.ID + ".txt")
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+
+		latitude, err := strconv.ParseFloat(data[0], 64)
+
+		if err != nil {
+			return nil
+		}
+
+		longitude, err := strconv.ParseFloat(data[1], 64)
+
+		if err != nil {
+			return nil
+		}
+
+		route.Positions = append(route.Positions, Position{
+			Latitude:  latitude,
+			Longitude: longitude,
+		})
+	}
+
+	return nil
 }
