8ee282203abfc24e2028201484feca934b5608e7
Mauricio <mauricio.cds00@gmail.com>
2023-02-07 13:11:51 -0300

    feat: kafka consumer done

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mauFade/go-simulator-del
 
 go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
---
+++ infra/kafka/consumer.go
@@ -0,0 +1,41 @@
+package kafka
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type KafkaConsumer struct {
+	MessageChannel chan *ckafka.Message
+}
+
+func (k *KafkaConsumer) Consume() {
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
+		"group.id":          os.Getenv("KafkaConsumerGroupId"),
+	}
+
+	consumer, err := ckafka.NewConsumer(configMap)
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	topics := []string{os.Getenv("KafkareadTopic")}
+
+	consumer.SubscribeTopics(topics, nil)
+
+	fmt.Println("Kafka consumer started.")
+
+	for {
+		msg, err := consumer.ReadMessage(-1)
+
+		if err == nil {
+			// message channel do Go recebe a mensagem do kafka
+			k.MessageChannel <- msg
+		}
+	}
+}
